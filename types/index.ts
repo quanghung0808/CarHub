3a47a0abf605699afb0ce8b870264a17cb7e436e
Hungdqse150123 <duongquanghung0122@gmail.com>
2024-01-02 08:41:05 +0700

    Car Hub version 1

---
+++ types/index.ts
@@ -0,0 +1,59 @@
+import { MouseEventHandler } from "react";
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+export interface HomeProps {
+  searchParams: FilterProps;
+}
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+  setLimit: (value: number) => void;
+}
+
+export interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+}
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  options: OptionProps[];
+}
+export interface CustomButtonProps {
+  isDisabled?: boolean;
+  btnType?: "button" | "submit";
+  containerStyles?: string;
+  textStyles?: string;
+  title: string;
+  rightIcon?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface SearchManufacturerProps {
+  selected?: string;
+  setSelected: (selected: string) => void;
+}
+
+export interface SearchBarProps {
+  setManufacturer: (model: string) => void;
+  setModel: (model: string) => void;
+}
