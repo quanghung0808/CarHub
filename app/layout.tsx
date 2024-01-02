dbdc24cc3f8cc669359c39aacf51b53d4a010fc4
Hungdqse150123 <duongquanghung0122@gmail.com>
2024-01-02 09:05:22 +0700

    Update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/model-icon.png" sizes="any" />
+      </head>
       <body className="relative">
         <Navbar />
         {children}
